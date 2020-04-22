d26de20b1f5c0b5db853b01dbba174bc9998ac4f
TheFallender (Agustin) <agustin-ferrari@hotmail.com>
2020-04-22 11:44:18 +0200

    Updates

---
+++ precache-manifest.0ede54a69ade4ec61bb9990d914fa186.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bab28fdc993c918e9d881ef9895ffb",
+    "url": "/mytube/index.html"
+  },
+  {
+    "revision": "dfa5f5675355ecd9842f",
+    "url": "/mytube/static/css/main.97268754.chunk.css"
+  },
+  {
+    "revision": "226ce442d1d927e5b23b",
+    "url": "/mytube/static/js/2.52ac4e14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytube/static/js/2.52ac4e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa5f5675355ecd9842f",
+    "url": "/mytube/static/js/main.71ff6bd6.chunk.js"
+  },
+  {
+    "revision": "3feaeeb8d6c3a2e95c6f",
+    "url": "/mytube/static/js/runtime-main.c9fa9c26.js"
+  },
+  {
+    "revision": "50cf4902a454d73207f072a66d139508",
+    "url": "/mytube/static/media/github.50cf4902.png"
+  },
+  {
+    "revision": "6f8f62b33512793a6a0989efa688363e",
+    "url": "/mytube/static/media/wrap.6f8f62b3.png"
+  }
+]);
